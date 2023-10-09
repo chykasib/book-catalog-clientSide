f261e5c3f087d4087779dbbf2bd52c486a224fa6
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-10-09 18:41:00 +0600

    added interface file

---
+++ src/interface/Ibook.ts
@@ -0,0 +1,9 @@
+export interface IBook {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _id: any;
+  title: string;
+  author: string;
+  genre: string;
+  publicationDate: string;
+  details: string;
+}
